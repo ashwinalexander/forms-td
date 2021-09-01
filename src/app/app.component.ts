f06b8af5f4c5334e24c3e1c5b919d816e61f96d5
ashwin alexander <ashwin@hauskey.com>
2021-08-31 23:51:59 -0400

    adding Angular forms validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,13 +7,25 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  @ViewChild('f') signUpForm: NgForm;
+
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
 
 
-  onSubmit(form: NgForm) {
-    console.log(form.value.email);
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
 
+  // }
+
+
+  onSubmit() {
+    console.log(this.signUpForm);
   }
+
+
+
 }
